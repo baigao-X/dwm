379e64408476c9d00345af01f9dbab362c22e866
baigao-X <1007668733@qq.com>
2025-07-12 17:50:21 +0800

    feat: 修改状态信息

--- scripts/statusbar/packages/cpu.sh
+++ scripts/statusbar/packages/cpu.sh
@@ -17,7 +17,7 @@ with_temp() {
 } 
 
 update() {
-    cpu_icon="閭"
+    cpu_icon=""
     cpu_text=$(top -n 1 -b | sed -n '3p' | awk '{printf "%02d%", 100 - $8}')
 
     icon=" $cpu_icon "
--- scripts/statusbar/packages/date.sh
+++ scripts/statusbar/packages/date.sh
@@ -11,18 +11,18 @@ signal=$(echo "^s$this^" | sed 's/_//')
 update() {
     time_text="$(date '+%m/%d %H:%M')"
     case "$(date '+%I')" in
-        "01") time_icon="" ;;
-        "02") time_icon="" ;;
-        "03") time_icon="" ;;
-        "04") time_icon="" ;;
-        "05") time_icon="" ;;
-        "06") time_icon="" ;;
-        "07") time_icon="" ;;
-        "08") time_icon="" ;;
-        "09") time_icon="" ;;
-        "10") time_icon="" ;;
-        "11") time_icon="" ;;
-        "12") time_icon="" ;;
+        "01") time_icon="󱐿" ;;
+        "02") time_icon="󱑀" ;;
+        "03") time_icon="󱑁" ;;
+        "04") time_icon="󱑂" ;;
+        "05") time_icon="󱑃" ;;
+        "06") time_icon="󱑄" ;;
+        "07") time_icon="󱑅" ;;
+        "08") time_icon="󱑆" ;;
+        "09") time_icon="󱑇" ;;
+        "10") time_icon="󱑈" ;;
+        "11") time_icon="󱑉" ;;
+        "12") time_icon="󱑊" ;;
     esac
 
     icon=" $time_icon "
